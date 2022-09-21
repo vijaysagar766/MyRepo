eb593d1d58b4e7ee939ba0469c7041d7b0ddee7c
Vijay <67680405+vijaysagar766@users.noreply.github.com>
2022-09-21 21:03:44 +0530

    Update DevClass.java

--- src/main/java/com/vijay/DevClass.java
+++ src/main/java/com/vijay/DevClass.java
@@ -2,6 +2,7 @@ package com.vijay;
 
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("Dev Class");
+        System.out.println("Dev Class+Stream2Learn");
+        //Checkin from Stream2 Learn
     }
 }
