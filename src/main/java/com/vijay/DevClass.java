fc215503bdc40e89a38944adc8184904b76813a8
Vijay <vijaysvijay77@gmail.com>
2022-09-21 20:33:58 +0530

    Initial Commit

---
+++ src/main/java/com/vijay/DevClass.java
@@ -0,0 +1,7 @@
+package com.vijay;
+
+public class DevClass {
+    public static void main(String[] args) {
+        System.out.println("Dev Class");
+    }
+}
---
+++ src/test/java/com/vijay/DevClassTest.java
@@ -0,0 +1,4 @@
+package com.vijay;
+
+public class DevClassTest {
+}
