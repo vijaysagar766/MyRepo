94e5b99dcccedb7ae6315cd2bdbca036103e9cdd
Vijay <vijaysvijay77@gmail.com>
2022-09-22 06:53:36 +0530

    Commit3

--- src/main/java/com/vijay/DevClass.java
+++ src/main/java/com/vijay/DevClass.java
@@ -3,5 +3,9 @@ package com.vijay;
 public class DevClass {
     public static void main(String[] args) {
         System.out.println("Dev Class");
+
+    Student s1 = new Student();
+        System.out.println(s1.getId());
+        System.out.println(s1.getName());
     }
 }
